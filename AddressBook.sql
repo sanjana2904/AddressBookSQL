f57179c6090b8eb0e1fd4ea57763cf320ca0c9b3
sanjana k <sanjanarthi@gmail.com>
2021-04-10 22:43:22 +0530

    Added UC9 - Alter AddressBook with Name and Type

--- AddressBook.sql
+++ AddressBook.sql
@@ -13,3 +13,16 @@ zip int,
 phoneNumber varchar(255),
 email varchar(255)
 );
+
+
+--UC9
+ALTER TABLE AddressbookTable ADD AddressBookName varchar(255), AddressBookType varchar(255);
+SELECT * FROM AddressBookTable;
+
+
+
+UPDATE AddressBookTable SET AddressBookType = 'family', AddressBookName='Book1' WHERE firstName='sanjana';
+UPDATE AddressBookTable SET AddressBookType = 'friends', AddressBookName='Book2' WHERE firstName='karthick';
+
+SELECT * FROM AddressBookTable where AddressBookType = 'family';
+SELECT * FROM AddressBookTable where AddressBookName = 'Book2';
